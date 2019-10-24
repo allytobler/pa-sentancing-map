95489de52a1a2844d6b9758d54c2f590205a413c
Adam <adam@adammarton.com>
2019-10-24 15:14:19 -0400

    pa map

---
+++ data2.js
@@ -0,0 +1,36 @@
+var data2 =
+{
+   "ADAMS": {
+      "total": 300,
+      "total_resentanced": 200,
+      "released": 50,
+      "resentanced_nonlife": 20,
+      "given_life": 10,
+      "waiting_resentance": 5
+   },
+    "ALLEGHENY": {
+    "total": 121,
+    "total_resentanced": 15,
+    "released": 9,
+    "resentanced_nonlife": 33,
+    "given_life": 354,
+    "waiting_resentance": 22
+    },
+    "ARMSTRONG": {
+    "total": 566,
+    "total_resentanced": 124,
+    "released": 172,
+    "resentanced_nonlife": 22,
+    "given_life": 90,
+    "waiting_resentance": 81
+  },
+
+    "BEAVER": {
+    "total": 0,
+    "total_resentanced": 0,
+    "released": 0,
+    "resentanced_nonlife": 0,
+    "given_life": 0,
+    "waiting_resentance": 0
+    }
+}
